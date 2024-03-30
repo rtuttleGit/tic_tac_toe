84b96e10ffb2baf953dacf2f25b4dbae3e113de0
M29002 <ryan.tuttle@cigna.com>
2024-03-30 14:14:48 -0400

    Initial Commit

---
+++ tic_tac_toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic_tac_toe
+//
+//  Created by M29002 on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
